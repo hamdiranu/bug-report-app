9a848da8d258f155bd44d95150ad4e5b218f157a
hamdiranu <hamdira@sci.ui.ac.id>
2023-09-27 17:09:17 +0700

    finished app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Bugs from "./components/Bugs";
+import BugsList from "./components/BugsList";
+import configureStore from "./store/configureStore";
+import { Provider } from "react-redux";
+
+const store = configureStore();
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BugsList />;
+    </Provider>
+  );
+}
+
+export default App;
